8c5e53ff15709e08c85f3432ba05a82294897907
unknown <fanaydee@gmail.com>
2024-05-20 11:11:00 +0500

    java

---
+++ java sript variebles/app.js
@@ -0,0 +1,34 @@
+let name = "irfan";
+name = "ullah";
+console.log (name)
+
+var name2 = "ullah"
+name2 = "irfan"
+console.log (name2)
+
+const name3 ="irfan ullah"
+// name3 ="irfan" not allowed in const
+console.log (name3)
+
+let fullName ="irfan ullah" //data type string
+fullName ="ali nawaz"
+console.log (typeof fullName)
+
+let fullName2 ='irfan ullah' //data type strings
+fullName2 ='naveed ali'
+console.log (typeof fullName2)
+
+let age = 23; //data type Number
+console.log (typeof age)
+
+let isMarried = true; //data type boolean
+console.log (typeof isMarried)
+
+let ispass = undefined; // undefined
+console.log (typeof ispass)
+
+let isFeesPaid = null;  // null 
+console.log (isFeesPaid)
+
+let stringNumber = "1234";
+console.log (typeof stringNumber)
